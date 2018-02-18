308e6978815f109ac17c5b5ccb41556f246239d0
Owner <Owner@10.0.0.135>
2018-02-18 00:45:42 -0500

    first change

--- java_basic/src/java_basic/java.java
+++ java_basic/src/java_basic/java.java
@@ -1,10 +1,11 @@
 package java_basic;
 
 public class java {
-	 static String name="ahsan";
+	 static String name="bristi";
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//String name="ahsan";
+		System.out.println(name);
 		System.out.println(name);
 
 	}
